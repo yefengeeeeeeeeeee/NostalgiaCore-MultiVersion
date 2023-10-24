385c89ab15d18a8bb33f6d682f7ceccd3ec62400
GameHerobrine <GameHerobrine@users.noreply.github.com>
2023-10-24 16:08:38 +0300

    tweaks

--- src/config.php
+++ src/config.php
@@ -48,11 +48,11 @@ set_include_path(get_include_path() . PATH_SEPARATOR . FILE_PATH);
 ini_set("memory_limit", "256M"); //Default
 define("LOG", true);
 define("START_TIME", microtime(true));
-define("MAJOR_VERSION", "1.1.0dev");
+define("MAJOR_VERSION", "1.1.0beta1");
 define("CODENAME", "懐かしさ (Nostalgia)"); //i'm not very creative - kotyaralih
 define("CURRENT_MINECRAFT_VERSION", "v0.8.1 alpha");
 define("CURRENT_API_VERSION", '12.1');
-define("CURRENT_PHP_VERSION", "5.5");
+define("CURRENT_PHP_VERSION", "8.0");
 $gitsha1 = false;
 if(file_exists(FILE_PATH . ".git/refs/heads/master")){ //Found Git information!
 	define("GIT_COMMIT", strtolower(trim(file_get_contents(FILE_PATH . ".git/refs/heads/master"))));
