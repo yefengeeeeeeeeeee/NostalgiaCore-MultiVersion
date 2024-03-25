3ab4f0f52f8b4e617a2d775f442eac028e1684db
GameHerobrine <GameHerobrine@users.noreply.github.com>
2024-03-25 22:59:57 +0300

    i forgit to save DandelionBlock

--- src/material/block/plant/DandelionBlock.php
+++ src/material/block/plant/DandelionBlock.php
@@ -3,7 +3,7 @@
 class DandelionBlock extends FlowableBlock{
 	public function __construct(){
 		parent::__construct(DANDELION, 0, "Dandelion");
-        $this->isActivable = true;
+		$this->isActivable = true;
 		$this->hardness = 0;
 	}
 
@@ -27,34 +27,34 @@ class DandelionBlock extends FlowableBlock{
 		return false;
 	}
 
-    public function onActivate(Item $item, Player $player){
-        if($item->getID() === DYE and $item->getMetadata() === 0x0F){
-            if(($player->gamemode & 0x01) === 0){
-                $player->removeItem(DYE,0x0F,1);
-            }
-            $random = new Random();
-            self::placeFlowers($this->level, new Vector3($this->x, $this->y, $this->z), $random, $random->nextRange(2, 5), 2);
-            return true;
-        }
-        return false;
-    }
+	public function onActivate(Item $item, Player $player){
+		if($item->getID() === DYE and $item->getMetadata() === 0x0F){
+			if(($player->gamemode & 0x01) === 0){
+				$player->removeItem(DYE,0x0F,1);
+			}
+			$random = new Random();
+			self::placeFlowers($this->level, new Vector3($this->x, $this->y, $this->z), $random, $random->nextRange(2, 5), 2);
+			return true;
+		}
+		return false;
+	}
 
-    public static function placeFlowers(Level $level, Vector3 $pos, Random $random, $count, $radius){
-        for($c = 0; $c < $count; ++$c){
-            $x = $random->nextRange($pos->x - $radius, $pos->x + $radius);
-            $z = $random->nextRange($pos->z - $radius, $pos->z + $radius);
-            for($y = $pos->y - 2; $y <= $pos->y + 2; ++$y){
-                if($level->level->getBlockID($x, $y + 1, $z) === AIR and $level->level->getBlockID($x, $y, $z) === GRASS){
-                    $changeFlower = $random->nextRange(1, 7);
-                    if($changeFlower === 1){
-                        $t = BlockAPI::get(CYAN_FLOWER, 0);
-                    } else {
-                        $t = BlockAPI::get(DANDELION, 0);
-                    }
-                    $level->setBlockRaw(new Vector3($x, $y + 1, $z), $t);
-                    break;
-                }
-            }
-        }
-    }
+	public static function placeFlowers(Level $level, Vector3 $pos, Random $random, $count, $radius){
+		for($c = 0; $c < $count; ++$c){
+			$x = $random->nextRange($pos->x - $radius, $pos->x + $radius);
+			$z = $random->nextRange($pos->z - $radius, $pos->z + $radius);
+			for($y = $pos->y - 2; $y <= $pos->y + 2; ++$y){
+				if($level->level->getBlockID($x, $y + 1, $z) === AIR and $level->level->getBlockID($x, $y, $z) === GRASS){
+					$changeFlower = $random->nextRange(1, 7);
+					if($changeFlower === 1){
+						$t = BlockAPI::get(CYAN_FLOWER, 0);
+					} else {
+						$t = BlockAPI::get(DANDELION, 0);
+					}
+					$level->setBlockRaw(new Vector3($x, $y + 1, $z), $t);
+					break;
+				}
+			}
+		}
+	}
 }
